58bdaf19e2ba5d95dd982bebdd87752c693e0dee
bankysalz <bankysalz@gmail.com>
2022-09-09 19:15:55 -0700

    sf

--- 0x00-hello_world/5-print.c
+++ 0x00-hello_world/5-print.c
@@ -1,15 +1,12 @@
-#include <stdio.h>                                                                                                                
-                                                                                                                                  
-/**                                                                                                                               
-* main - print the string in the put function                                                                                     
-*                                                                                                                                 
-*Description: using the main function                                                                                             
-* this program prints "with proper grammar, but the outcome is a piece of art,                                                    
-*return: 0                                                                                                                        
-*/                                                                                                                                
-int main(void)                                                                                                                    
-{                                                                                                                                 
-        printf("with proper grammar, but the outcome is a piece of art,\n");                                                      
-        return (0);                                                                                                               
-}                      
+#include <stdio.h>
 
+/**
+ * main - entry point for the program.
+ *
+ * Return: 0 if no errors, return non zero if errors
+	 */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
